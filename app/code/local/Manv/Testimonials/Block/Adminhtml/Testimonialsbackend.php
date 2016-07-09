433ccc737aeac7c623a681d481d3540db713b33a
shriekz <ssrikanth77@gmail.com>
2016-07-09 08:13:07 +0530

    removed unwated files

--- app/code/local/Manv/Testimonials/Block/Adminhtml/Testimonialsbackend.php
+++
@@ -1,5 +0,0 @@
-<?php  
-
-class Manv_Testimonials_Block_Adminhtml_Testimonialsbackend extends Mage_Adminhtml_Block_Template {
-
-}
--- app/code/local/Manv/Testimonials/Model/Mysql4/Testimonials.php
+++
@@ -1,8 +0,0 @@
-<?php
-class Manv_Testimonials_Model_Mysql4_Testimonials extends Mage_Core_Model_Mysql4_Abstract
-{
-    protected function _construct()
-    {
-        $this->_init("testimonials/testimonials", "id");
-    }
-}
